f810d55342ac927c919cfa54f0597f86b3aa2822
willy <williman.nicolas@gmail.com>
2021-04-11 02:31:16 -0300

    agregado tipo de prenda

--- Prenda.java
+++ Prenda.java
@@ -2,8 +2,20 @@ class Prenda {
 
   private Estado estado;
 
+  private String tipo;
+
   private Float precioBase;
-  
+
+  public Prenda(String tipo) {
+    List<String> tiposDePrenda = ["saco", "pantalon", "camisa"];
+
+    if (!tiposDePrenda.contains(tipo)) {
+      throw new Error('Tipo de prenda invalido');
+    }
+
+    this.tipo = tipo;
+  }
+
   public Float precio() {
     return this.estado.calcularPrecio(this.precioBase);
   }
